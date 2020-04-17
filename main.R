0d5a0fc860a1a3359ef540e15474fe5f9e242b2a
Jovita Gudan <gud.jovita@gmail.com>
2020-04-17 12:53:14 +0300

    testuoju (nekreipk demesio)

--- main.R
+++ main.R
@@ -22,7 +22,7 @@ assign("basisfd", basisfd, getNamespace('fda'))
 unlockBinding("eval.penalty", getNamespace('fda'))
 assign("eval.penalty", eval.penalty, getNamespace('fda'))
 
-
+#nuskaitymas
 dat = read.csv(
   '../COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 )
