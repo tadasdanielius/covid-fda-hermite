29e097b4aceb096b6f7273250c070d39e72e7256
Tadas <rigus@fractal.lt>
2020-05-14 16:36:52 +0300

    few changes

--- pop_norm.R
+++ pop_norm.R
@@ -117,10 +117,28 @@ map.world$Cluster <- rep('Others', dim(map.world)[1])
 map.world[map.world$region %in% lt_cluster_countries,'Cluster'] = 'LT Cluster'
 
 
-ggplot() + geom_polygon(data = map.world, aes(x = long, y = lat, group = group, fill = Cluster))
+ggplot() + geom_polygon(data = map.world, aes(x = long, y = lat, group = group, fill = Cluster)) + 
+  theme(text = element_text(family = "Gill Sans", color = "#FFFFFF")
+        ,panel.background = element_rect(fill = "#444444")
+        ,plot.background = element_rect(fill = "#444444")
+        ,panel.grid = element_blank()
+        ,plot.title = element_text(size = 30)
+        ,plot.subtitle = element_text(size = 10)
+        ,axis.text = element_blank()
+        ,axis.title = element_blank()
+        ,axis.ticks = element_blank()
+        ,legend.position = "none"
+  )
+
 
 
 
 f <- fdata(fddat)
 dep.f <- depth.FM(f)
 plot(dep.f)
+
+
+
+
+plot(fddat[clst == clst['Lithuania']])
+clst
