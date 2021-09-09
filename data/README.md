860d7294d5cacfbaa001c5fbc7c09aa9aa695aa9
Jovita Gudan <gud.jovita@gmail.com>
2021-09-09 11:55:26 +0300

    Updated dataset list and a description of the data is attached

---
+++ data/README.md
@@ -0,0 +1,32 @@
+
+
+# Data on COVID-19 (coronavirus) 
+
+All data sets analyzed in this research where taken from [_Our World in Data_](https://ourworldindata.org/coronavirus).
+
+
+
+## Global Covid-19 data
+
+### Datasets metadata
+
+Stored in [`data_import.R`](data_import.R)
+
+* `cases_countries`: total confirmed cases of COVID-19 for 193 countries.
+* `cases_world`: total confirmed cases of COVID-19 of a world.
+* `cases_continents`: total confirmed cases of COVID-19 for 6 continents.
+* `cases_norm_countries`: total confirmed cases of COVID-19 per 100,000 inhabitants for 193 countries.
+* `cases_norm_world`: total confirmed cases of COVID-19 per 100,000 inhabitants of a world.
+* `cases_norm_continents`: total confirmed cases of COVID-19 per 100,000 inhabitants for 6 continents.
+* `deaths_countries`: total deaths attributed to COVID-19 for 193 countries.
+* `deaths_world`:  total deaths attributed to COVID-19 of a world.
+* `deaths_continents`:  total deaths attributed to COVID-19 for 6 continents.
+* `deaths_norm_countries`: total deaths attributed to COVID-19 per 100,000 inhabitants for 193 countries.
+* `deaths_norm_world`: total deaths attributed to COVID-19 per 100,000 inhabitants of a world.
+* `deaths_norm_continents`: total deaths attributed to COVID-19 per 100,000 inhabitants for 6 continents. 
+* `vacc_countries`: total number of people who received at least one vaccine dose for 193 countries.
+* `vacc_world`: total number of people who received at least one vaccine dose of a world.
+* `vacc_continents`: total number of people who received at least one vaccine dose for 6 continents.
+* `vacc_norm_countries`: total number of people who received at least one vaccine dose per 100,000 inhabitants for 193 countries.
+* `vacc_norm_world`: total number of people who received at least one vaccine dose per 100,000 inhabitants of a world.
+* `vacc_norm_continents`: total number of people who received at least one vaccine dose per 100,000 inhabitants for 6 continents. 
--- data/data_import.R
+++ data/data_import.R
@@ -1,25 +1,64 @@
 
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+
+long_to_wide <- function(long_table, column_value){
+  
+  #sort table
+  long_table <- long_table[order(long_table$location, long_table$date),]
+  
+  #columns and rows
+  date_range <- seq(min(long_table[,"date"]), max(long_table[,"date"]), by=1)
+  unq_loc <- unique(long_table[,"location"])
+  
+  #create wider table
+  wider_table <- matrix(NA, nrow=length(unique(long_table[,"location"])), ncol=length(date_range))
+  colnames(wider_table) <- as.character(date_range)
+  rownames(wider_table) <- unq_loc
+  
+  for(loc_j in 1:length(unq_loc)){
+    tmp_data <- long_table %>% filter(location==  unq_loc[loc_j])
+    dt <- data.frame(column_value = tmp_data[,column_value])
+    
+    tmp_data[,column_value] <- nafill(dt, type = "locf")[[1]]
+    tmp_location <- which(rownames(wider_table) %in% unq_loc[loc_j])
+    for(i in 1:length(tmp_data$date)){
+      wider_table[tmp_location,which(colnames(wider_table) %in% as.character(tmp_data$date[i]))] <- tmp_data[i,column_value]
+    }
+  }
+  wider_table <- as.data.frame(wider_table)
+}
+
+
+
+
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 
 
 cases_per_pop = 100000
 
-loc_table_owid <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/locations.csv"))
-head(loc_table_owid)
+pop_table_owid <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/owid/covid-19-data/master/scripts/input/un/population_2020.csv"))
+head(pop_table_owid)
 
-pop <- loc_table_owid[, c("location","population")]
+pop <- pop_table_owid[, c("entity","population")]
+colnames(pop)[1] <- "location"
 
-
-################################################################
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 world <- c("World")
 continents <- c("Africa", "Asia", "Europe", "North America", "South America", "Oceania")
 
 
-####### vaccinated ################################################
+#>>>>>>>> Vaccinated <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 vac_table_owid <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"))
 head(vac_table_owid)
 
+vac_table_owid$people_vaccinated <- as.numeric(vac_table_owid$people_vaccinated)
+
 vac_table_owid <- merge(pop,vac_table_owid,by=c("location"), all.y=T)
 vac_table_owid$vacc_normalized = vac_table_owid$people_vaccinated / (vac_table_owid$population/cases_per_pop)
+
+
 
 #not countries
 not_countries_list <- unique(vac_table_owid[which(grepl("^OWID", vac_table_owid$iso_code)), c("location", "iso_code")])
@@ -27,101 +66,84 @@ not_countries_list <- rbind(not_countries_list, c("2020 Summer Olympics athletes
 
 vac_table_C <- vac_table_owid[which(!(vac_table_owid$location %in% not_countries_list$location)),]
 
-vac_table_C <- vac_table_C[order(vac_table_C$location, vac_table_C$date),]
+#vac_table_C <- vac_table_C[order(vac_table_C$location, vac_table_C$date),]
+
 
 ##### Totals ######
-#vaccinated countries
+# [vaccinated countries]
 vac_tmp <- vac_table_C[,c("people_vaccinated", "location", "date")]
-suppressWarnings({vac_tmp[,"people_vaccinated"] <- as.integer(vac_tmp[,"people_vaccinated"])})
-
-date_range <- seq(min(vac_tmp[,"date"]), max(vac_tmp[,"date"]), by=1)
-unq_countries <- unique(vac_tmp[,"location"])
-
-vacc_countries <- matrix(NA, nrow=length(unique(vac_tmp[,"location"])), ncol=length(date_range))
-colnames(vacc_countries) <- as.character(date_range)
-rownames(vacc_countries) <- unq_countries
-
-for(cntry_j in 1:length(unq_countries)){
-  
-  tmp_data <- vac_tmp %>% filter(location==  unq_countries[cntry_j])
-  dt <- data.frame("people_vaccinated" = tmp_data[,"people_vaccinated"])
-  
-  tmp_data[,"people_vaccinated"] <- as.integer(nafill(dt, type = "locf")[[1]])
-  tmp_location <- which(rownames(vacc_countries) %in% unq_countries[cntry_j])
-  for(i in 1:length(tmp_data$date)){
-    vacc_countries[tmp_location,which(colnames(vacc_countries) %in% as.character(tmp_data$date[i]))] <- tmp_data$people_vaccinated[i]
-  }
-}
-vacc_countries <- as.data.frame(vacc_countries)
-
-
+vacc_countries <- long_to_wide(vac_tmp, "people_vaccinated")
 # matplot(t(vacc_countries), type="l")
 
 
+vacc_countries[which(vacc_countries$`2021-09-07` %in% max(na.omit(vacc_countries$`2021-09-07`))),]
+
+##### Per 1000,000 inhabitants ######
+# [vaccinated countries per 100,000 inhabitant]
+vac_tmp_norm <- vac_table_C[,c("vacc_normalized", "location", "date")]
+vacc_norm_countries <- long_to_wide(vac_tmp_norm, "vacc_normalized")
+# matplot(t(vacc_norm_countries), type="l")
+
+#100% vaccinated
+vacc_norm_countries[which(vacc_norm_countries$`2021-09-07` %in% max(na.omit(vacc_norm_countries$`2021-09-07`))),]
+#Gibraltar exceeds 100% due to vaccination of non-residents
+vacc_norm_countries[which(vacc_norm_countries$`2021-09-01` %in% max(na.omit(vacc_norm_countries$`2021-09-01`))),]
+
+
+##### Totals ######
 #vaccinated world
 vacc_world0 <- vac_table_owid[which(vac_table_owid$location %in% world),c("people_vaccinated", "location", "date")]
-vacc_world <- t(vacc_world0$people_vaccinated)
-colnames(vacc_world) <- as.character(vacc_world0$date)
-rownames(vacc_world) <- unique(vacc_world0$location)
-
-#vaccinated continents
-vacc_cont <- vac_table_owid[which(vac_table_owid$location %in% continents),c("people_vaccinated", "location", "date")]
-suppressWarnings({vacc_cont[,"people_vaccinated"] <- as.integer(vacc_cont[,"people_vaccinated"])})
-date_range <- seq(min(vacc_cont[,"date"]), max(vacc_cont[,"date"]), by=1)
-
-
-vac_continents <- matrix(NA, nrow=length(unique(vacc_cont[,"location"])), ncol=length(date_range))
-colnames(vac_continents) <- as.character(date_range)
-rownames(vac_continents) <- continents
-
-for(cont_j in 1:length(continents)){
-  tmp_data <- vacc_cont %>% filter(location==  continents[cont_j])
-  dt <- data.frame("people_vaccinated" = tmp_data[,"people_vaccinated"])
-  
-  tmp_data[,"people_vaccinated"] <- as.integer(nafill(dt, type = "locf")[[1]])
-  tmp_location <- which(rownames(vac_continents) %in% continents[cont_j])
-  for(i in 1:length(tmp_data$date)){
-    vac_continents[tmp_location,which(colnames(vac_continents) %in% as.character(tmp_data$date[i]))] <- tmp_data$people_vaccinated[i]
-  }
-}
-vac_continents[is.na(vac_continents)] <- 0
-vac_continents <- as.data.frame(vac_continents)
+vacc_world <- long_to_wide(vacc_world0, "people_vaccinated")
+# matplot(t(vacc_world), type="l")
 
 
 ##### Per 1000,000 inhabitants ######
-#vaccinated countries
+# vaccinated world per 100,000 inhabitant
+vacc_world_norm0 <- vac_table_owid[which(vac_table_owid$location %in% world),c("vacc_normalized", "location", "date")]
+vacc_norm_world <- long_to_wide(vacc_world_norm0, "vacc_normalized")
+# matplot(t(vacc_norm_world), type="l")
 
 
-tmp_vacc <- vacc_countries
-tmp_vacc$location <- rownames(vacc_countries)
-pop_vac <- merge(tmp_vacc, pop, by=("location"), all.x=T)
+##### Totals ######
+#vaccinated continents
+vacc_cont <- vac_table_owid[which(vac_table_owid$location %in% continents),c("people_vaccinated", "location", "date")]
+vacc_continents <- long_to_wide(vacc_cont, "people_vaccinated")
+vacc_continents[is.na(vacc_continents)] <- 0
+# matplot(t(vacc_continents), type="l")
 
-vacc_norm_countries <-  pop_vac[, 2:(dim(pop_vac)[2]-1) ] / (pop_vac$population/cases_per_pop)
-rownames(vacc_norm_countries) <- pop_vac$location
-
-# matplot(t(vacc_norm_countries), type="l")
+#Asia (data for China added onJun 10, Aug 12, Aug 26)
+vacc_continents[which(vacc_continents$`2021-09-07` %in% max(na.omit(vacc_continents$`2021-09-07`))),]
 
 
+##### Per 1000,000 inhabitants ######
+#vaccinated continents per 100,000 inhabitant
+vacc_cont_norm <- vac_table_owid[which(vac_table_owid$location %in% continents),c("vacc_normalized", "location", "date")]
+vacc_norm_continents <- long_to_wide(vacc_cont_norm, "vacc_normalized")
+vacc_norm_continents[is.na(vacc_norm_continents)] <- 0
+# matplot(t(vacc_norm_continents), type="l")
 
-#############Confirmed cases
+
+
+
+
+
+# >>>>>>>>>>>>>>  Confirmed cases <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 cases_table_owid <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/total_cases.csv"))
 head(cases_table_owid)
 
+
+# <<<<<<<<<<<<<< TOTALS >>>>>>>>>>>>>>>>>>>>>>>>
 #cases countries
 cases_c <- cases_table_owid[,-which(colnames(cases_table_owid) %in% not_countries_list$location)]
 cases_countries <- t(cases_c[,-which(colnames(cases_c)  %in% "date")])
 colnames(cases_countries) <- as.character(cases_c$date)
 cases_countries <- as.data.frame(cases_countries)
+# matplot(t(cases_countries), type="l")
 
-#cases countries per 100,000 inhabitants
-tmp_vacc <- cases_countries
-tmp_vacc$location <- rownames(cases_countries)
-pop_vac <- merge(tmp_vacc, pop, by=("location"), all.x=T)
+#France 
+# matplot(t(cases_countries[which(rownames(cases_countries) %in% "France"),]), type="l")
+# cases_countries[which(rownames(cases_countries) %in% "France"),]
 
-cases_norm_countries <-  pop_vac[, 2:(dim(pop_vac)[2]-1) ] / (pop_vac$population/cases_per_pop)
-rownames(cases_norm_countries) <- pop_vac$location
-
-# matplot(t(cases_norm_countries), type="l")
 
 
 #cases world
@@ -130,22 +152,84 @@ cases_world <- t(cases_world0[,-which(colnames(cases_world0)  %in% "date")])
 colnames(cases_world) <- as.character(cases_world0$date)
 rownames(cases_world) <- colnames(cases_world0)[2]
 cases_world <- as.data.frame(cases_world)
+# matplot(t(cases_world), type="l")
 
-#cases continents
+
+# [cases continents]
 cases_cont <- cases_table_owid[,c("date",continents)]
 cases_continents <- t(cases_cont[,-which(colnames(cases_cont)  %in% "date")])
 colnames(cases_continents) <- as.character(cases_cont$date)
 cases_continents <- as.data.frame(cases_continents)
+# matplot(t(cases_continents), type="l")
 
-#############Death cases
+
+# <<<<<<<<<<<<<< per 100,000 inhabitants >>>>>>>>>>>>>>>>>>>>>>>>
+
+# [cases countries per 100,000 inhabitants]
+tmp_vacc <- cases_countries
+tmp_vacc$location <- rownames(cases_countries)
+pop_vac <- merge(tmp_vacc, pop, by=("location"), all.x=T)
+
+cases_norm_countries <-  pop_vac[, 2:(dim(pop_vac)[2]-1) ] / (pop_vac$population/cases_per_pop)
+rownames(cases_norm_countries) <- pop_vac$location
+# matplot(t(cases_norm_countries), type="l")
+
+
+# [cases world per 100,000 inhabitants]
+
+cases_world0 <- cases_table_owid[,c("date", world)]
+cases_world0 <- pivot_longer(cases_world0, cols= !("date"), names_to = "location", values_to = "cases")
+cases_world0 <- merge(pop,cases_world0,by=c("location"), all.y=T)
+cases_world0$cases_norm <- cases_world0$cases / (cases_world0$population/cases_per_pop)
+cases_world0 <- cases_world0[,c("location", "date", "cases_norm")]
+cases_norm_world <- long_to_wide(cases_world0, "cases_norm")
+# matplot(t(cases_norm_world), type="l")
+
+# [cases continents per 100,000 inhabitants]
+cases_cont <- cases_table_owid[,c("date",continents)]
+cases_cont <- pivot_longer(cases_cont, cols= !("date"), names_to = "location", values_to = "cases")
+cases_cont <- merge(pop,cases_cont,by=c("location"), all.y=T)
+cases_cont$cases_norm <- cases_cont$cases / (cases_cont$population/cases_per_pop)
+cases_cont <- cases_cont[order(cases_cont$location, cases_cont$date),]
+cases_cont <- cases_cont[,c("location", "date", "cases_norm")]
+cases_norm_continents <- long_to_wide(cases_cont, "cases_norm")
+# matplot(t(cases_norm_continents), type="l")
+
+
+
+# >>>>>>>>>>>>>  Death cases <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 deaths_table_owid <- as.data.frame(data.table::fread("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/jhu/total_deaths.csv"))
 head(deaths_table_owid)
 
+
+# <<<<<<<<<<<<<< TOTALS >>>>>>>>>>>>>>>>>>>>>>>>
 #deaths countries
 deathes_c <- deaths_table_owid[,-which(colnames(deaths_table_owid) %in% not_countries_list$location)]
 deaths_countries <- t(deathes_c[,-which(colnames(deathes_c)  %in% "date")])
 colnames(deaths_countries) <- as.character(deathes_c$date)
 deaths_countries <- as.data.frame(deaths_countries)
+# matplot(t(deaths_countries), type="l")
+
+
+#deaths world
+deaths_world0 <- deaths_table_owid[,c("date", world)]
+deaths_world <- t(deaths_world0[,-which(colnames(deaths_world0)  %in% "date")])
+colnames(deaths_world) <- as.character(cases_world0$date)
+rownames(deaths_world) <- colnames(deaths_world0)[2]
+deaths_world <- as.data.frame(deaths_world)
+# matplot(t(deaths_world), type="l")
+
+
+#deaths continents
+deaths_cont <- deaths_table_owid[,c("date",continents)]
+deaths_continents <- t(deaths_cont[,-which(colnames(deaths_cont)  %in% "date")])
+colnames(deaths_continents) <- as.character(deaths_cont$date)
+deaths_continents <- as.data.frame(deaths_continents)
+# matplot(t(deaths_continents), type="l")
+
+
+
+# <<<<<<<<<<<<<< per 100,000 inhabitants >>>>>>>>>>>>>>>>>>>>>>>>
 
 #cases countries per 100,000 inhabitants
 tmp_vacc <- deaths_countries
@@ -154,23 +238,49 @@ pop_vac <- merge(tmp_vacc, pop, by=("location"), all.x=T)
 
 deaths_norm_countries <-  pop_vac[, 2:(dim(pop_vac)[2]-1) ] / (pop_vac$population/cases_per_pop)
 rownames(deaths_norm_countries) <- pop_vac$location
-
 # matplot(t(deaths_norm_countries), type="l")
 
-#deaths world
+
+# [deaths world per 100,000 inhabitants]
 deaths_world0 <- deaths_table_owid[,c("date", world)]
-deaths_world <- t(deaths_world0[,-which(colnames(deaths_world0)  %in% "date")])
-colnames(deaths_world) <- as.character(cases_world0$date)
-rownames(deaths_world) <- colnames(deaths_world0)[2]
-deaths_world <- as.data.frame(deaths_world)
+deaths_world0 <- pivot_longer(deaths_world0, cols= !("date"), names_to = "location", values_to = "cases")
+deaths_world0 <- merge(pop,deaths_world0,by=c("location"), all.y=T)
+deaths_world0$cases_norm <- deaths_world0$cases / (deaths_world0$population/cases_per_pop)
+deaths_world0 <- deaths_world0[,c("location", "date", "cases_norm")]
+deaths_norm_world <- long_to_wide(deaths_world0, "cases_norm")
+# matplot(t(deaths_norm_world), type="l")
 
-#deaths continents
+
+
+# [deaths continent per 100,000 inhabitants]
+
 deaths_cont <- deaths_table_owid[,c("date",continents)]
-deaths_continents <- t(deaths_cont[,-which(colnames(deaths_cont)  %in% "date")])
-colnames(deaths_continents) <- as.character(deaths_cont$date)
-deaths_continents <- as.data.frame(deaths_continents)
+deaths_cont <- pivot_longer(deaths_cont, cols= !("date"), names_to = "location", values_to = "cases")
+deaths_cont <- merge(pop,deaths_cont,by=c("location"), all.y=T)
+deaths_cont$cases_norm <- deaths_cont$cases / (deaths_cont$population/cases_per_pop)
+deaths_cont <- deaths_cont[order(deaths_cont$location, deaths_cont$date),]
+deaths_cont <- deaths_cont[,c("location", "date", "cases_norm")]
+deaths_norm_continents <- long_to_wide(deaths_cont, "cases_norm")
+# matplot(t(deaths_norm_continents), type="l")
 
-# matplot(t(deaths_continents), type="l")
 
 
-
+rm(list=setdiff(ls(), c("cases_countries", # [cases countries]
+                        "cases_world", # [cases world]
+                        "cases_continents",  # [cases continents]
+                        "cases_norm_countries", # [cases countries per 100,000 inhabitants]
+                        "cases_norm_world", # [cases world per 100,000 inhabitants]
+                        "cases_norm_continents",  # [cases continents per 100,000 inhabitants]
+                        "deaths_countries", # [deaths countries]
+                        "deaths_world", # [deaths world]
+                        "deaths_continents",  # [deaths continents]
+                        "deaths_norm_countries", # [deaths countries per 100,000 inhabitants]
+                        "deaths_norm_world", # [deaths world per 100,000 inhabitants]
+                        "deaths_norm_continents",  # [deaths continents per 100,000 inhabitants]
+                        "vacc_countries", # [vaccinated countries]
+                        "vacc_world", # [vaccinated world]
+                        "vacc_continents", # [vaccinated continents]
+                        "vacc_norm_countries", # [vaccinated countries per 100,000 inhabitants]
+                        "vacc_norm_world", # [vaccinated world per 100,000 inhabitants]
+                        "vacc_norm_continents" # [vaccinated continents per 100,000 inhabitants]
+                        )))
